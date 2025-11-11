abaf92a4b2a9130a39b76e6790ec2319f9836028
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2025-11-10 20:22:58 -0500

    add comment

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")  
-
 results = model.train(data="./Flower/flower.yaml", epochs=100)
 
-te = model("torenia_01.jpg",save=True)
+#afer trainning, trained model will be saved at runs/detect/train../weights
+
+# model = YOLO("runs/dectect/train5/weights/best.pt") 
+# te = model("torenia_01.jpg",save=True)
