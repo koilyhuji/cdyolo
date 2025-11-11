2f6bf18b80d785d101cf4c4ffc444cea4ac0c64d
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2025-11-11 00:16:45 -0500

    change dataset

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-#model = YOLO("yolo11n.pt")  
-# results = model.train(data="./Flower/flower.yaml", epochs=100)
+model = YOLO("yolo11n.pt")  
+results = model.train(data="./Flower/flower.yaml", epochs=100)
 
 #afer trainning, trained model will be saved at runs/detect/train../weights
 
