6d8200590b5022cf65ef3b5aaf7427c6ef7a1040
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2025-11-11 00:15:13 -0500

    change dataset

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")  
-results = model.train(data="./Flower/flower.yaml", epochs=100)
+#model = YOLO("yolo11n.pt")  
+# results = model.train(data="./Flower/flower.yaml", epochs=100)
 
 #afer trainning, trained model will be saved at runs/detect/train../weights
 
-# model = YOLO("runs/dectect/train5/weights/best.pt") 
-# te = model("torenia_01.jpg",save=True)
+model = YOLO("runs/detect/train6/weights/last.pt") 
+te = model("flowerhh.jpg",save=True,conf=0.1)
